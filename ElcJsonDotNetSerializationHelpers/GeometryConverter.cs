d8caf6d944e7617cfc2b03e7320ac2628db11010
JeffJacobson <JeffJacobson@users.noreply.github.com>
2015-09-15 10:54:02 -0700

    Backported serialization helper
    Copied from master branch the serialization helper that adds
    compatibility with JSON.NET.

---
+++ ElcJsonDotNetSerializationHelpers/GeometryConverter.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using Wsdot.Geometry.Contracts;
+
+namespace Wsdot.Elc.Serialization
+{
+	/// <summary>
+	/// Allows a <see cref="GeometryContract"/> (an abstract class) to be deserialized by <see href="http://json.net/">Json.NET</see>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <remarks>This code comes from Tom DuPoint .NET <see href="http://www.tomdupont.net/2014/04/deserialize-abstract-classes-with.html"/>.</remarks>
+	public abstract class AbstractJsonConverter<T> : JsonConverter
+	{
+
+		protected abstract T Create(Type objectType, JObject jObject);
+
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(T).IsAssignableFrom(objectType);
+		}
+
+
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var jObject = JObject.Load(reader);
+
+			T target = Create(objectType, jObject);
+			serializer.Populate(jObject.CreateReader(), target);
+
+			return target;
+		}
+
+
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+
+
+
+		protected static bool FieldExists(JObject jObject, string name, JTokenType type)
+		{
+			JToken token;
+			return jObject.TryGetValue(name, out token) && token.Type == type;
+		}
+
+	}
+
+	public class GeometryConverter : AbstractJsonConverter<GeometryContract>
+	{
+		protected override GeometryContract Create(Type objectType, JObject jObject)
+		{
+			if (FieldExists(jObject, "rings", JTokenType.Array))
+			{
+				return new PolygonContract();
+			}
+			else if (FieldExists(jObject, "paths", JTokenType.Array))
+			{
+				return new PolylineContract();
+			}
+			else if (FieldExists(jObject, "xmin", JTokenType.Float))
+			{
+				return new EnvelopeContract();
+			}
+			else if (FieldExists(jObject, "points", JTokenType.Array))
+			{
+				return new MultipointContract();
+			}
+			else if (FieldExists(jObject, "x", JTokenType.Float))
+			{
+				return new PointContract();
+			}
+
+			throw new InvalidOperationException("Could not determine geometry type.");
+		}
+	}
+}
---
+++ SerializationTest/SerializationUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using SerializationTest.Properties;
+using System.Linq;
+using Wsdot.Elc.Contracts;
+using Wsdot.Elc.Serialization;
+using Wsdot.Geometry.Contracts;
+
+namespace SerializationTest
+{
+	[TestClass]
+	public class SerializationUnitTest
+	{
+		[TestMethod]
+		public void DeserializePoint()
+		{
+			const double 
+				x = -122.345, 
+				y = 48.72345;
+			const int wkid = 4326;
+			string json = string.Format("{{\"x\":{0}, \"y\":{1}, \"spatialReference\": {{\"wkid\":{2}}} }}", x, y, wkid);
+			var point = JsonConvert.DeserializeObject<PointContract>(json);
+			const string shouldBeFmt = "{0} should be {1}";
+			Assert.AreEqual(x, point.X, shouldBeFmt, "X", x);
+			Assert.AreEqual(y, point.Y, shouldBeFmt, "Y", y);
+			Assert.AreEqual(wkid, point.SpatialReference.Wkid, shouldBeFmt, "SpatialReference.Wkid", wkid);
+
+			var converter = new GeometryConverter();
+
+			var geom = JsonConvert.DeserializeObject<GeometryContract>(json, converter);
+			Assert.IsInstanceOfType(geom, typeof(PointContract));
+		}
+
+		[TestMethod]
+		public void DeserializeRouteLocationsArray()
+		{
+			string json = Resources.RouteLocations;
+			var converter = new GeometryConverter();
+
+			var locations = JsonConvert.DeserializeObject<RouteLocation[]>(json, converter);
+			CollectionAssert.AllItemsAreNotNull(locations, "Items in array should not be null.");
+			Assert.AreEqual(1, locations.Length, "Array should have a single element.");
+			var location = locations.First();
+			Assert.AreEqual(155.49741407457302, location.Angle);
+			Assert.AreEqual(0, location.Arm);
+			Assert.AreEqual(0, location.ArmCalcReturnCode);
+			Assert.IsFalse(location.Back.Value);
+			Assert.IsFalse(location.Decrease.Value);
+			Assert.IsInstanceOfType(location.EventPoint, typeof(PointContract));
+		}
+	}
+}
